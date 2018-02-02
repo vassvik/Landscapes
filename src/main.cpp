f2add104490f0be7364c230e56fbe53b6d7a838b
unknown <mvassvik@gmail.com>
2018-02-02 23:47:36 +0100

    Small correction

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main() {
 	//
 	//glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	//
