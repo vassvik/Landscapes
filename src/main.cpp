0b2d2e2e1c6c9e82445fbec1d67dace8e545fb87
vassvik <mvassvik@gmail.com>
2018-05-11 14:20:24 +0200

    spaces
    spaces

--- src/main.cpp
+++ src/main.cpp
@@ -9,111 +9,111 @@
  
 
 int main() {
-	//
-	if (!glfwInit()) {
-		printf("Error, could not initialize GLFW\n");
-		return -1;
-	}
+    //
+    if (!glfwInit()) {
+        printf("Error, could not initialize GLFW\n");
+        return -1;
+    }
 
-	//
-	//glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //
+    //glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//
-	int resx = 1000, resy = 1000;
-	GLFWwindow *window = glfwCreateWindow(resx, resy, "Terrain Visualizer", NULL, NULL);
+    //
+    int resx = 1000, resy = 1000;
+    GLFWwindow *window = glfwCreateWindow(resx, resy, "Terrain Visualizer", NULL, NULL);
 
-	//
-	if (window == NULL) {
-		printf("Error, could not create window.\n");
-		glfwTerminate();
-		return -2;
-	}
+    //
+    if (window == NULL) {
+        printf("Error, could not create window.\n");
+        glfwTerminate();
+        return -2;
+    }
 
-	//
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // Enable vsync
+    //
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
 
-	//
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		printf("Could not load OpenGL functions.\n");
-		return -3;
-	}
+    //
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Could not load OpenGL functions.\n");
+        return -3;
+    }
 
-	ImGui::CreateContext();
-	ImGui_ImplGlfwGL3_Init(window, true);
-	//ImGui::StyleColorsDark();
+    ImGui::CreateContext();
+    ImGui_ImplGlfwGL3_Init(window, true);
+    //ImGui::StyleColorsDark();
 
-	// 
-	GLuint program = load_shaders("src/vertex_shader.glsl", "src/fragment_shader.glsl");
+    // 
+    GLuint program = load_shaders("src/vertex_shader.glsl", "src/fragment_shader.glsl");
 
-	//
-	Mesh mesh;
-	int Nx = 20;
-	int Ny = 20;
+    //
+    Mesh mesh;
+    int Nx = 20;
+    int Ny = 20;
 
-	int num_vertices = (Nx+1)*(Ny+1);
-	Vec3 *base_vertices = new Vec3[3*num_vertices];
+    int num_vertices = (Nx+1)*(Ny+1);
+    Vec3 *base_vertices = new Vec3[3*num_vertices];
 
-	float dx = 0.05;
-	float dy = 0.05;
-	float min_x = 0.0;
-	float min_y = 0.0;
-	float max_x = dx*Nx; 
-	float max_y = dy*Ny;
-	for (int j = 0; j <= Ny; j++) {
-		for (int i = 0; i <= Nx; i++) {
-			int k = j*(Nx+1) + i;
-			float x = min_x + float(i)*dx - (max_x + min_x)/2.0;
-			float y = min_y + float(j)*dy - (max_y + min_y)/2.0;
-			base_vertices[k] = Vec3(x, y, 0.25*cos(2.0*3.14*x)*cos(2.0*3.14*y) + 0.125*cos(4.0*3.14*x+ 3.7*y)*cos(4.0*3.14*y - 6.0*x));
-		}
-	}
+    float dx = 0.05;
+    float dy = 0.05;
+    float min_x = 0.0;
+    float min_y = 0.0;
+    float max_x = dx*Nx; 
+    float max_y = dy*Ny;
+    for (int j = 0; j <= Ny; j++) {
+        for (int i = 0; i <= Nx; i++) {
+            int k = j*(Nx+1) + i;
+            float x = min_x + float(i)*dx - (max_x + min_x)/2.0;
+            float y = min_y + float(j)*dy - (max_y + min_y)/2.0;
+            base_vertices[k] = Vec3(x, y, 0.25*cos(2.0*3.14*x)*cos(2.0*3.14*y) + 0.125*cos(4.0*3.14*x+ 3.7*y)*cos(4.0*3.14*y - 6.0*x));
+        }
+    }
 
-	mesh.num_vertices = Nx*Ny*6;
-	mesh.vertices = new Vertex[mesh.num_vertices];
+    mesh.num_vertices = Nx*Ny*6;
+    mesh.vertices = new Vertex[mesh.num_vertices];
 
-	for (int j = 0; j < Ny; j++) {
-		for (int i = 0; i < Nx; i++) {
-			int k = j*Nx + i;
+    for (int j = 0; j < Ny; j++) {
+        for (int i = 0; i < Nx; i++) {
+            int k = j*Nx + i;
 
-			int i0 = i + 0;
-			int i1 = i + 1;
-			int j0 = j + 0;
-			int j1 = j + 1;
+            int i0 = i + 0;
+            int i1 = i + 1;
+            int j0 = j + 0;
+            int j1 = j + 1;
 
-			int k00 = j0*(Nx+1) + i0;
-			int k10 = j0*(Nx+1) + i1;
-			int k01 = j1*(Nx+1) + i0;
-			int k11 = j1*(Nx+1) + i1;
+            int k00 = j0*(Nx+1) + i0;
+            int k10 = j0*(Nx+1) + i1;
+            int k01 = j1*(Nx+1) + i0;
+            int k11 = j1*(Nx+1) + i1;
 
-			Vec3 v00 = base_vertices[k00];
-			Vec3 v10 = base_vertices[k10];
-			Vec3 v01 = base_vertices[k01];
-			Vec3 v11 = base_vertices[k11];
+            Vec3 v00 = base_vertices[k00];
+            Vec3 v10 = base_vertices[k10];
+            Vec3 v01 = base_vertices[k01];
+            Vec3 v11 = base_vertices[k11];
 
-			Vec3 n0 = normalize(cross(v10 - v00, v11 - v00));
-			Vec3 n1 = normalize(cross(v11 - v00, v01 - v00));
+            Vec3 n0 = normalize(cross(v10 - v00, v11 - v00));
+            Vec3 n1 = normalize(cross(v11 - v00, v01 - v00));
 
-			mesh.vertices[6*k+0] = Vertex{v00, n0, k % 11};
-			mesh.vertices[6*k+1] = Vertex{v10, n0, k % 11};
-			mesh.vertices[6*k+2] = Vertex{v11, n0, k % 11};
-			mesh.vertices[6*k+3] = Vertex{v00, n1, k % 11};
-			mesh.vertices[6*k+4] = Vertex{v11, n1, k % 11};
-			mesh.vertices[6*k+5] = Vertex{v01, n1, k % 11};
-		}
-	}
+            mesh.vertices[6*k+0] = Vertex{v00, n0, k % 11};
+            mesh.vertices[6*k+1] = Vertex{v10, n0, k % 11};
+            mesh.vertices[6*k+2] = Vertex{v11, n0, k % 11};
+            mesh.vertices[6*k+3] = Vertex{v00, n1, k % 11};
+            mesh.vertices[6*k+4] = Vertex{v11, n1, k % 11};
+            mesh.vertices[6*k+5] = Vertex{v01, n1, k % 11};
+        }
+    }
 
 
-	// 
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+    // 
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*mesh.num_vertices, &mesh.vertices[0], GL_STATIC_DRAW);
@@ -173,7 +173,7 @@ int main() {
 
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-	int hover_triangle = -1;
+    int hover_triangle = -1;
     Vec3 hover_position;
 
     int clicked_triangle = -1;
@@ -181,45 +181,45 @@ int main() {
 
     float clicked_radius = 0.1;
 
-	//
-	glClearColor(1.0, 0.7, 0.4 , 1.0);
-	while (!glfwWindowShouldClose(window)) {
-		// let GLFW handle OS events and update internal input data
-		glfwPollEvents();
+    //
+    glClearColor(1.0, 0.7, 0.4 , 1.0);
+    while (!glfwWindowShouldClose(window)) {
+        // let GLFW handle OS events and update internal input data
+        glfwPollEvents();
 
-		// close the window at the end of the frame if Escape is pressed
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-			glfwSetWindowShouldClose(window, GLFW_TRUE);
-		}
+        // close the window at the end of the frame if Escape is pressed
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
 
         ImGui_ImplGlfwGL3_NewFrame();
 
         ImGuiIO &io = ImGui::GetIO();
 
-		//
-		glUseProgram(program);
-		glUniform1f(glGetUniformLocation(program, "time"), glfwGetTime());
+        //
+        glUseProgram(program);
+        glUniform1f(glGetUniformLocation(program, "time"), glfwGetTime());
 
-		// render to framebuffer
-		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // render to framebuffer
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//
-		glBindVertexArray(vao);
+        //
+        glBindVertexArray(vao);
 
 
-		glUniform1i(glGetUniformLocation(program, "wireframe"), 0);
-		glPolygonMode(GL_FRONT_AND_BACK , GL_FILL);
-		glDrawArrays(GL_TRIANGLES, 0, mesh.num_vertices);
+        glUniform1i(glGetUniformLocation(program, "wireframe"), 0);
+        glPolygonMode(GL_FRONT_AND_BACK , GL_FILL);
+        glDrawArrays(GL_TRIANGLES, 0, mesh.num_vertices);
 
-		glUniform1i(glGetUniformLocation(program, "wireframe"), 1);
-		glPolygonMode(GL_FRONT_AND_BACK , GL_LINE);
-		glDrawArrays(GL_TRIANGLES, 0, mesh.num_vertices);
+        glUniform1i(glGetUniformLocation(program, "wireframe"), 1);
+        glPolygonMode(GL_FRONT_AND_BACK , GL_LINE);
+        glDrawArrays(GL_TRIANGLES, 0, mesh.num_vertices);
 
-		glPolygonMode(GL_FRONT_AND_BACK , GL_FILL);
+        glPolygonMode(GL_FRONT_AND_BACK , GL_FILL);
 
-		//
-		glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
+        //
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
         glReadBuffer(GL_COLOR_ATTACHMENT1);
 
         //
@@ -235,78 +235,78 @@ int main() {
         bool hit_terrain = !((fabs(data[0] - 1.0) < 1.0e-4 && fabs(data[1] - 0.7) < 1.0e-4 && fabs(data[2] - 0.4) < 1.0e-4));
 
         if (hit_terrain) {
-        	hover_triangle = int(data[0] + 0.5);
-        	hover_position = Vec3{data[1], data[2], data[3]};
+            hover_triangle = int(data[0] + 0.5);
+            hover_position = Vec3{data[1], data[2], data[3]};
         } else {
-        	hover_triangle = -1;
-        	hover_position = Vec3{0.0, 0.0, 0.0};
+            hover_triangle = -1;
+            hover_position = Vec3{0.0, 0.0, 0.0};
         }
 
         if (hover_triangle != -1) {
-        	ImGui::Text("Hover: %d at {%f %f %f}", hover_triangle, hover_position.x, hover_position.y, hover_position.z);
+            ImGui::Text("Hover: %d at {%f %f %f}", hover_triangle, hover_position.x, hover_position.y, hover_position.z);
         } else {
-        	ImGui::Text("Hover: None");
+            ImGui::Text("Hover: None");
         }
         
 
         if (ImGui::IsMouseClicked(0)) {
-        	clicked_triangle = int(data[0] + 0.5);
-        	clicked_position = Vec3{data[1], data[2], data[3]};
+            clicked_triangle = int(data[0] + 0.5);
+            clicked_position = Vec3{data[1], data[2], data[3]};
         } else if (ImGui::IsMouseReleased(0)) {
-        	clicked_triangle = int(-1);
-        	clicked_position = Vec3{0.0, 0.0, 0.0};
+            clicked_triangle = int(-1);
+            clicked_position = Vec3{0.0, 0.0, 0.0};
         }
 
         if (clicked_triangle != -1) {
-        	ImGui::Text("Clicked: %d at {%f %f %f}", clicked_triangle, clicked_position.x, clicked_position.y, clicked_position.z);
+            ImGui::Text("Clicked: %d at {%f %f %f}", clicked_triangle, clicked_position.x, clicked_position.y, clicked_position.z);
         } else {
-        	ImGui::Text("Clicked: None");
+            ImGui::Text("Clicked: None");
         }
 
-	    if (io.KeyCtrl) {
-	        clicked_radius += ImGui::GetIO().MouseWheel*0.01;
-	        if (clicked_radius < 0.0) clicked_radius = 0.0;
-	        if (clicked_radius > 0.5) clicked_radius = 0.5;
-	    }
+        if (io.KeyCtrl) {
+            clicked_radius += ImGui::GetIO().MouseWheel*0.01;
+            if (clicked_radius < 0.0) clicked_radius = 0.0;
+            if (clicked_radius > 0.5) clicked_radius = 0.5;
+        }
 
-    	// draw
-	    static float dist = 4.0; 
-	    static float phi = 45.0; 
-	    static float theta = 90.0;   
-	    float fov = 45.0; 
+        // draw
+        static float dist = 4.0; 
+        static float phi = 45.0; 
+        static float theta = 90.0;   
+        float fov = 45.0; 
 
-	    if (io.MouseDown[0]) {   
-	    	phi -= io.MouseDelta.x;     
-		    theta -= io.MouseDelta.y;
-	        if (theta > 179.0) theta = 179.0;
-	        if (theta < 1.0) theta = 1.0;
-	    } 
+        if (io.MouseDown[0]) {   
+            phi -= io.MouseDelta.x;     
+            theta -= io.MouseDelta.y;
+            if (theta > 179.0) theta = 179.0;
+            if (theta < 1.0) theta = 1.0;
+        } 
 
-	    if (!io.KeyCtrl)
-    		dist *= pow(0.95, io.MouseWheel); 
+        if (!io.KeyCtrl)
+            dist *= pow(0.95, io.MouseWheel); 
 
-	    Vec3 pos = dist*Vec3(cos(PI/180.0*phi)*sin(PI/180.0*theta), sin(PI/180.0*phi)*sin(PI/180.0*theta), cos(PI/180.0*theta));
-	    Vec3 at = Vec3(0.0, 0.0, 0.0);
-	    Vec3 f = normalize(Vec3(at.x - pos.x, at.y - pos.y, at.z - pos.z));
-	    Vec3 u = normalize(Vec3(0.0, 0.0, 1.0));
-	    Vec3 r = normalize(cross(f,u));
-	    u = cross(r,f);
+        Vec3 pos = dist*Vec3(cos(PI/180.0*phi)*sin(PI/180.0*theta), sin(PI/180.0*phi)*sin(PI/180.0*theta), cos(PI/180.0*theta));
+        Vec3 at = Vec3(0.0, 0.0, 0.0);
+        Vec3 f = normalize(Vec3(at.x - pos.x, at.y - pos.y, at.z - pos.z));
+        Vec3 u = normalize(Vec3(0.0, 0.0, 1.0));
+        Vec3 r = normalize(cross(f,u));
+        u = cross(r,f);
 
-	    Mat4 View = look_at(pos, at, Vec3(0.0, 0.0, 1.0));    
-	    Mat4 Projection = perspective(fov, resx/float(resy), 0.1, 1000.0);
-	    Mat4 Model;
+        Mat4 View = look_at(pos, at, Vec3(0.0, 0.0, 1.0));    
+        Mat4 Projection = perspective(fov, resx/float(resy), 0.1, 1000.0);
+        Mat4 Model;
 
-	    Mat4 MVP = Projection*View*Model;    
-	    glUniformMatrix4fv(glGetUniformLocation(program, "M"), 1, GL_FALSE, &Model.M[0][0]); 
-	    glUniformMatrix4fv(glGetUniformLocation(program, "MVP"), 1, GL_FALSE, &MVP.M[0][0]); 
+        Mat4 MVP = Projection*View*Model;    
+        glUniformMatrix4fv(glGetUniformLocation(program, "M"), 1, GL_FALSE, &Model.M[0][0]); 
+        glUniformMatrix4fv(glGetUniformLocation(program, "MVP"), 1, GL_FALSE, &MVP.M[0][0]); 
 
-    	glUniform1i(glGetUniformLocation(program, "hover_triangle"), hover_triangle);
-    	glUniform1i(glGetUniformLocation(program, "clicked_triangle"), clicked_triangle);
+        glUniform1i(glGetUniformLocation(program, "hover_triangle"), hover_triangle);
+        glUniform1i(glGetUniformLocation(program, "clicked_triangle"), clicked_triangle);
 
-    	glUniform3f(glGetUniformLocation(program, "hover_position"), hover_position.x, hover_position.y, hover_position.z);
-    	glUniform3f(glGetUniformLocation(program, "clicked_position"), clicked_position.x, clicked_position.y, clicked_position.z);
+        glUniform3f(glGetUniformLocation(program, "hover_position"), hover_position.x, hover_position.y, hover_position.z);
+        glUniform3f(glGetUniformLocation(program, "clicked_position"), clicked_position.x, clicked_position.y, clicked_position.z);
 
-    	glUniform1f(glGetUniformLocation(program, "clicked_radius"), clicked_radius);
+        glUniform1f(glGetUniformLocation(program, "clicked_radius"), clicked_radius);
 
         //
         glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
@@ -322,15 +322,14 @@ int main() {
 
 
         //
-		glfwSwapBuffers(window);
-	}
+        glfwSwapBuffers(window);
+    }
 
-	//
-	ImGui_ImplGlfwGL3_Shutdown();
+    //
+    ImGui_ImplGlfwGL3_Shutdown();
     ImGui::DestroyContext();
-	glfwTerminate();
+    glfwTerminate();
 
-	//
-	return 0;
+    //
+    return 0;
 }
-
