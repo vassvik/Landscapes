a45622c45594c5ab301df8f0cd237fb398a8dd0d
unknown <mvassvik@gmail.com>
2018-05-23 23:25:33 +0200

    updated build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
-cl /nologo /Iinclude /std:c++latest /EHsc src\main.cpp src\utils.cpp src\imgui_impl_glfw_gl3.cpp include\glad\glad.c lib\glfw3.lib gdi32.lib user32.lib libcmt.lib shell32.lib lib\imgui.lib
+cl /nologo /Iinclude /std:c++latest /EHsc /MP ^
+	src\main.cpp ^
+	src\utils.cpp ^
+	src\imgui_impl_glfw_gl3.cpp ^
+	include\glad\glad.c ^
+	lib\glfw3.lib gdi32.lib user32.lib libcmt.lib shell32.lib lib\imgui.lib
