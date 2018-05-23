18e485ffd8200faba150cae10f4dc8c8342d85a7
unknown <mvassvik@gmail.com>
2018-05-24 00:13:54 +0200

    Downgraded to glad 3.3

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-cl /nologo /Iinclude /std:c++latest /EHsc /MP ^
+cl /nologo /Iinclude /std:c++latest /EHsc /MP /Zi ^
 	src\main.cpp ^
 	src\utils.cpp ^
 	src\imgui_impl_glfw_gl3.cpp ^
 	include\glad\glad.c ^
-	lib\glfw3.lib gdi32.lib user32.lib libcmt.lib shell32.lib lib\imgui.lib
+	/DEBUG lib\glfw3.lib gdi32.lib user32.lib libcmt.lib shell32.lib lib\imgui.lib
